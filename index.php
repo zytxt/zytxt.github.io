fbf6d03dabea8709cffdd3af9995b1c7a0a63ae7
zytxt <90083673+zytxt@users.noreply.github.com>
2021-09-18 12:28:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,19 @@
+
 <!DOCTYPE html>
-
 <html lang="en-US">
-
   <head>
-
-	<link rel="icon" type="image/png" href="favicon.png" />	<meta charset="UTF-8">
-
+	<link rel="icon" type="image/png" href="favicon.png" />
+	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
 	<title>励文档</title>
-
   </head>
-
   <body>
 
-	<center>
+<center>
+	 <p>
+    <strong><span style="font-size: 15px; color: rgb(89, 89, 89);">励文档</span></strong><br/>
+</p>
 
-	  <h1>励文档</h1>
 
 	</center>
-
- 
-
- 
-
- 
-
-  </body>
-
 </html>
-
-<!--下面是内容-->
